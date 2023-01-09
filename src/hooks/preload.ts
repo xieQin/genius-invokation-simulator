7b9d177243d18e69d5c5fee35525672bd8557e79
cherish <xieqin1991@gmail.com>
2023-01-09 15:32:01 +0800

    fix: fix images

--- src/hooks/preload.ts
+++ src/hooks/preload.ts
@@ -7,9 +7,9 @@ import characters from "@/data/characters.json";
 export const usePreload = () => {
   const [loading, setLoading] = useState(true);
 
-  const cardImgs = cards.map(card => `${PUBLIC_PATH}cards/${card.imgID}.png`);
+  const cardImgs = cards.map(card => `${PUBLIC_PATH}/cards/${card.imgID}.png`);
   const characterImgs = characters.map(
-    character => `${PUBLIC_PATH}characters/${character.imgID}.png`
+    character => `${PUBLIC_PATH}/characters/${character.imgID}.png`
   );
   // const cardImgs = cards.map(card => card.img);
   // const characterImgs = characters.map(character => character.img);
