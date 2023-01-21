acc27ee50fcb4c9470eb25e5cc1b7d8cc2f82fa4
cherish <xieqin1991@gmail.com>
2023-01-21 10:13:16 +0800

    chore: fix code style

--- src/views/Game/skill.tsx
+++ src/views/Game/skill.tsx
@@ -1,7 +1,7 @@
 import { CostDiceZone } from "@/components/DiceZone";
 import GameLayer from "@/components/GameLayer";
-import { useCostDice, useSkill, useTimeout } from "@/hooks";
-import { Action, Phase, PlayerPosition } from "@/models";
+import { useCostDice, useSkill } from "@/hooks";
+import { Phase, PlayerPosition } from "@/models";
 import { useGameStore } from "@/stores";
 
 export default function SkillPhase() {
@@ -20,7 +20,6 @@ export default function SkillPhase() {
 
   const character = getPlayer(pos).characters[activeCharacters[pos]];
 
-  console.log(actions);
   const onConfirm = () => {
     const costs = character.skills[activeSkills[pos]].costs;
     if (isCostValid(costs)) {
