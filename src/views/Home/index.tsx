c0086a34ec5932e9ee3cca4e96b81f7cccf4c75d
cherish <xieqin1991@gmail.com>
2023-01-23 21:54:02 +0800

    feat: deck styles

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,3 +1,11 @@
+import styles from "./index.module.css";
+
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className={styles.Home}>
+        An unofficial simulator for Genius Invokation TCG in Genshin Impact
+      </div>
+    </div>
+  );
 }
