16d516958b9875bb0cc4f7acebe654aaac2dc38e
cherish <xieqin1991@gmail.com>
2023-02-04 10:28:59 +0800

    fix: fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Suspense, useEffect } from "react";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import setupIndexedDB from "use-indexeddb";
 
 import { idbConfig } from "./configs/indexdb";
+import { Routes } from "./routes";
 
 export default function App() {
   useEffect(() => {
